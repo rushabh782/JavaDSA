dcbe5ca8561c44d60b89d065f84d92f6a612a755
rushabh782 <155352920+rushabh782@users.noreply.github.com>
2024-08-22 21:43:32 +0530

    added code for singleton.java

---
+++ OOPS/singleton/Singleton.java
@@ -0,0 +1,11 @@
+package Kunal.JavaOOPS.singleton;
+
+public class Singleton {
+    public static void main(String[] args) {
+//        Singleton obj = Singleton.getInstance();
+//        Singleton obj2 = Singleton.getInstance();
+//        Singleton obj3 = Singleton.getInstance();
+
+        //all 3 ref variables are pointing to same object
+    }
+}
