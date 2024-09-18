0a59de6c5c1d6dbffe912d507bf40272c8f8dfab
rushabh782 <155352920+rushabh782@users.noreply.github.com>
2024-09-18 14:29:59 +0530

    added 2 files for linked list

---
+++ LinkedList/LL.java
@@ -0,0 +1,81 @@
+package Kunal.LinkedList;
+
+public class LL {
+
+    private Node head;
+    private Node tail;
+    private int size;
+    public LL(){
+        this.size = 0;
+    }
+
+    //Inserting new node at start
+    public void insertFirst(int val){
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+
+        if (tail == null){
+            tail = head;
+        }
+
+        size += 1;
+    }
+
+    //Inserting new node at end
+    public void insertLast(int val){
+        if(tail == null){
+            insertFirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+    //Inserting new node at particular index
+    public void insert(int val,int index){
+        if(index == 0){
+            insertFirst(val);
+            return;
+        }
+        if (index == size){
+            insertLast(val);
+            return;
+        }
+        Node temp = head;
+        for (int i = 1; i < index; i++){
+            temp = temp.next;
+        }
+
+        Node node = new Node(val,temp.next);
+        temp.next = node;
+
+        size++;
+    }
+
+    public void display(){
+        Node temp = head;
+        while (temp != null){
+            System.out.print(temp.value+ " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
+
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+        public Node(int value,Node next){
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
---
+++ LinkedList/Main.java
@@ -0,0 +1,15 @@
+package Kunal.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(2);
+        list.insertFirst(8);
+        list.insertFirst(17);
+        list.insertLast(99);
+
+        list.insert(100,3);
+        list.display();
+    }
+}
